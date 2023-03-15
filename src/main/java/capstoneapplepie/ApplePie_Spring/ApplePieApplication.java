04190388b7af77f017dfc31a65fd57d234d54b11
12hyeon <hyeonjeong1212@naver.com>
2023-03-16 00:58:59 +0900

    init : create project

---
+++ src/main/java/capstoneapplepie/ApplePie_Spring/ApplePieApplication.java
@@ -0,0 +1,13 @@
+package capstoneapplepie.ApplePie_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplePieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplePieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstoneapplepie/ApplePie_Spring/ApplePieApplicationTests.java
@@ -0,0 +1,13 @@
+package capstoneapplepie.ApplePie_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplePieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
